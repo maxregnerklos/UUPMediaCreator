7f5f4927d8dba2da42077d46acf07a2c1d8ee243
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-05-06 17:05:07 +0200

    uupmediav2
    Redone work quick

---
+++ src/Applications/UUPDownload/BuildTargets.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright (c) Gustave Monce and Contributors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnifiedUpdatePlatform.Media.Creator.Planning;
+using UnifiedUpdatePlatform.Services.Composition.Database;
+using UnifiedUpdatePlatform.Services.Temp;
+using UnifiedUpdatePlatform.Services.WindowsUpdate;
+
+namespace UUPDownload
+{
+    public static class BuildTargets
+    {
+        public class EditionPlanningWithLanguage
+        {
+            public List<EditionTarget> EditionTargets;
+            public string LanguageCode;
+        }
+
+        public static async Task<EditionPlanningWithLanguage> GetTargetedPlanAsync(this UpdateData update, string LanguageCode)
+        {
+            HashSet<CompDB> compDBs = await update.GetCompDBsAsync();
+            Package editionPackPkg = compDBs.GetEditionPackFromCompDBs();
+
+            string editionPkg = await update.DownloadFileFromDigestAsync(editionPackPkg.Payload.PayloadItem.First(x => !x.Path.EndsWith(".psf")).PayloadHash);
+            return await update.GetTargetedPlanAsync(LanguageCode, editionPkg);
+        }
+
+        public static async Task<EditionPlanningWithLanguage> GetTargetedPlanAsync(this UpdateData update, string LanguageCode, string editionPkg)
+        {
+            HashSet<CompDB> compDBs = await update.GetCompDBsAsync();
+            if (string.IsNullOrEmpty(editionPkg))
+            {
+                return null;
+            }
+
+            _ = ConversionPlanBuilder.GetTargetedPlan(compDBs, editionPkg, LanguageCode, true, out List<EditionTarget> targets, new TempManager(), null);
+            return new EditionPlanningWithLanguage() { EditionTargets = targets, LanguageCode = LanguageCode };
+        }
+
+        public static void PrintAvailablePlan(this List<EditionTarget> targets)
+        {
+            foreach (EditionTarget target in targets)
+            {
+                foreach (string str in ConversionPlanBuilder.PrintEditionTarget(target))
+                {
+                    Logging.Log(str);
+                }
+            }
+        }
+    }
+}
---
+++ src/Applications/UUPDownload/UpdateUtils.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Copyright (c) Gustave Monce and Contributors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnifiedUpdatePlatform.Services.Composition.Database;
+using UnifiedUpdatePlatform.Services.WindowsUpdate;
+
+namespace UUPDownload
+{
+    public static class UpdateUtils
+    {
+        public static string GetFilenameForCEUIFile(CExtendedUpdateInfoXml.File file2, IEnumerable<PayloadItem> payloadItems)
+        {
+            string filename = file2.FileName.Replace('\\', Path.DirectorySeparatorChar);
+            if (payloadItems.Any(x => x.PayloadHash == file2.AdditionalDigest.Text || x.PayloadHash == file2.Digest))
+            {
+                PayloadItem payload = payloadItems.First(x => x.PayloadHash == file2.AdditionalDigest.Text || x.PayloadHash == file2.Digest);
+                return payload.Path.Replace('\\', Path.DirectorySeparatorChar);
+            }
+            else if (!payloadItems.Any() && filename.Contains('_') && !filename.StartsWith("_") && (!filename.Contains('-') || filename.IndexOf('-') > filename.IndexOf('_')))
+            {
+                filename = filename[..filename.IndexOf('_')] + Path.DirectorySeparatorChar + filename[(filename.IndexOf('_') + 1)..];
+                return filename.TrimStart(Path.DirectorySeparatorChar);
+            }
+            return filename;
+        }
+
+        public static bool ShouldFileGetDownloaded(CExtendedUpdateInfoXml.File file2, IEnumerable<PayloadItem> payloadItems)
+        {
+            string filename = file2.FileName.Replace('\\', Path.DirectorySeparatorChar);
+            if (payloadItems.Any(x => x.PayloadHash == file2.AdditionalDigest.Text || x.PayloadHash == file2.Digest))
+            {
+                PayloadItem payload = payloadItems.First(x => x.PayloadHash == file2.AdditionalDigest.Text || x.PayloadHash == file2.Digest);
+                filename = payload.Path.Replace('\\', Path.DirectorySeparatorChar);
+
+                if (payload.PayloadType.Equals("ExpressCab", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // This is a diff cab, skip it
+                    return false;
+                }
+            }
+
+            return !filename.Contains("Diff", StringComparison.InvariantCultureIgnoreCase) &&
+!filename.Contains("Baseless", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static UpdateData TrimDeltasFromUpdateData(UpdateData update)
+        {
+            update.Xml.Files.File = update.Xml.Files.File.Where(x => !x.FileName.Replace('\\', Path.DirectorySeparatorChar).EndsWith(".psf", StringComparison.InvariantCultureIgnoreCase)
+            && !x.FileName.Replace('\\', Path.DirectorySeparatorChar).StartsWith("Diff", StringComparison.InvariantCultureIgnoreCase)
+             && !x.FileName.Replace('\\', Path.DirectorySeparatorChar).StartsWith("Baseless", StringComparison.InvariantCultureIgnoreCase)).ToArray();
+            return update;
+        }
+    }
+}
